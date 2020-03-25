01553b1c5833e56038ba045414d2daa44b7913b8
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2020-03-25 16:24:09 +0100

    Add maven example

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 docker build -t "jdk11" .
-docker run -v $(pwd)"/probe-aspectj":/home/kiekertest -it jdk11
+docker run \
+	-v $(pwd)"/probe-aspectj":/home/kiekertest \
+	-v $(pwd)"/aspectj-maven":/home/aspectj-maven \
+	-it jdk11
